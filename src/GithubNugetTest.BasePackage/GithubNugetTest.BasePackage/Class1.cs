6c9d9aabc0e756daae59067ba91ac7f708b8c9c4
mesuttasci <mesuttascic@gmail.com>
2023-06-19 12:13:35 +0300

    Initial Class Library

---
+++ src/GithubNugetTest.BasePackage/GithubNugetTest.BasePackage/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace GithubNugetTest.BasePackage
+{
+    public class BaseClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public DateTime ModifiedOnUtc { get; set; }
+    }
+}
